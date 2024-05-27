c1e0930574cdcb083e66b0c23148a6f926fcfca9
RenatoAugusto17 <169068426+RenatoAugusto17@users.noreply.github.com>
2024-05-27 22:53:23 +0000

    27/05/2024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,24 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("O programa a seguir calculará uma Velocidade Média");
-    int 
+    System.out.println("Distância:");
+    double distancia = 500;
+    System.out.println(distancia);
+    System.out.println("Hora gasta:");
+    double tempo = 7;
+      System.out.println(tempo);
+System.out.println("");
+
+    System.out.println("A velocidade Média é:");
+    System.out.println("Distância ÷ tempo gasto");
+    System.out.println("");
+double VelocidadeMédia = distancia/tempo;
+  System.out.println("Resultado da divisão:");
+    System.out.println(VelocidadeMédia);
+    System.out.println("");
+    System.out.println("Resultado final:");
+    System.out.println("71,4km/h");
+    
   }
 
   
